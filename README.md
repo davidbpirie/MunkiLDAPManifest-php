e327efae9197609d1888f6e60b063dfe80e42b77
David Pirie <davidbpirie@mac.com>
2022-03-25 11:36:55 +1100

    Expanded README and example_index.php

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # MunkiLDAPManifest-php
 Manage Munki manifest content with LDAP via Computers and Groups objects.
+
+## Overview
+MunkiLDAPManifest-php provides a mechanism for delivering dynamically-generated munki manifest files by using LDAP objects - Computers objects for the device to receive the manifest, and Groups objects for the item to be included.
+
+## Installation
+1. Create a composer.json file in your manifests directory containing:
+```
+{
+    "repositories": [
+        {
+            "type": "vcs",
+            "url": "https://github.com/davidbpirie/MunkiLDAPManifest-php.git"
+        }
+    ],
+    "require": {
+        "davidbpirie/munkildapmanifest-php": "dev-master"
+    }
+}
+```
+
+2. Install using composer:
+
+`composer install`
+
+Note: you will need to have composer already installed. See https://getcomposer.org/doc/00-intro.md for instructions.
+
+3. Copy the provided example_index.php to your manifests directory:
+
+`cp vendor/DavidBPirie/MunkiLDAPManifest-php/example_index.php index.php`
+
+4. Modify index.php to suit your environment - see the inline comments for guidance.
+
+## Client configuration
+
+Clients just need to have ManifestURL configured to point to your index.php file with a ? suffix eg:
+
+`defaults write /Library/Preferences/ManagedInstalls ManifestURL -string "https://munki.example.com/manifests/index.php?"`
+
+## Testing
+
+On your server, you can run the index.php file via php's CLI providing the client identitifer as an argument to verify the output for that host, eg:
+
+`php index.php client1.example.com`
--- example_index.php
+++ example_index.php
@@ -2,8 +2,60 @@
 namespace DavidBPirie\MunkiLDAPManifestPHP;
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/vendor/rodneyrehm/plist/classes/CFPropertyList/CFPropertyList.php';
 
+// LDAP connection config
+$ldap_server    = "ldapserver.example.com";
+$ldap_username  = "username";
+$ldap_password  = "password";
+$ldap_basedn    = "OU=Munki,DC=example,DC=com";
+
+// LDAP manifest component config
+/*
+Each manifest_component entry must include:
+    Name - what area it appears under in the manifest
+    DNPrefix - where to search for the LDAP objects
+    GroupPrefix - what each LDAP group name will be prefixed with
+Optionally can also include:
+    LDAPAttribute - where the value is stored in the LDAP group,
+        eg url. If not provided, the group name suffix is used.
+Only include manifest_components for the components you want
+to manage from within LDAP.
+*/
+
+$manifest_components = array(
+    array(
+        "Name"              => "catalogs",
+        "DNPrefix"          => "OU=Catalogs,",
+        "GroupPrefix"       => "Munki_catalog_"
+    ),
+    array(
+        "Name"              => "managed_installs",
+        "DNPrefix"          => "OU=Managed Installs,",
+        "GroupPrefix"       => "Munki_managed_install_"
+    ),
+    array(
+        "Name"              => "included_manifests",
+        "DNPrefix"          => "OU=Included Manifests,",
+        "GroupPrefix"       => "Munki_included_manifest_"
+    ),
+    array(
+        "Name"              => "managed_uninstalls",
+        "DNPrefix"          => "OU=Managed Uninstalls,",
+        "GroupPrefix"       => "Munki_managed_uninstall_"
+    ),
+    array(
+        "Name"              => "optional_installs",
+        "DNPrefix"          => "OU=Optional Installs,",
+        "GroupPrefix"       => "Munki_optional_install_"
+    ),
+    array(
+        "Name"              => "managed_updates",
+        "DNPrefix"          => "OU=Managed Updates,",
+        "GroupPrefix"       => "Munki_managed_update_"
+    )
+);
+
+// Allow running via cli providing $query_string as an argument 
 if( php_sapi_name() == "cli" ) {
 	if( $argc < 2 ) {
 		throw new \Exception("No query string provided." );
@@ -14,6 +66,7 @@ if( php_sapi_name() == "cli" ) {
 }
 
 if( strstr( $query_string, "/" ) ) {
+    // $query_strings including a "/" are interpreted as requesting the contents of a file
     if( file_exists( $query_string ) ) {
         echo file_get_contents( $query_string );
     } else {
@@ -21,52 +74,31 @@ if( strstr( $query_string, "/" ) ) {
         die();
     }
 } else {
-	$client_id = $query_string;
-    
-    $manifest_components = array(
-        array(
-            "Name"              => "catalogs",
-            "DNPrefix"          => "OU=Catalogs,",
-            "GroupPrefix"       => "Munki_catalog_"
-        ),
-        array(
-            "Name"              => "managed_installs",
-            "DNPrefix"          => "OU=Managed Installs,",
-            "GroupPrefix"       => "Munki_managed_install_"
-        ),
-        array(
-            "Name"              => "included_manifests",
-            "DNPrefix"          => "OU=Included Manifests,",
-            "GroupPrefix"       => "Munki_included_manifest_"
-        ),
-        array(
-            "Name"              => "managed_uninstalls",
-            "DNPrefix"          => "OU=Managed Uninstalls,",
-            "GroupPrefix"       => "Munki_managed_uninstall_"
-        ),
-        array(
-            "Name"              => "optional_installs",
-            "DNPrefix"          => "OU=Optional Installs,",
-            "GroupPrefix"       => "Munki_optional_install_"
-        ),
-        array(
-            "Name"              => "managed_updates",
-            "DNPrefix"          => "OU=Managed Updates,",
-            "GroupPrefix"       => "Munki_managed_update_"
-        )
-    );
-    
+    // $query_strings not including a "/" are interpreted as a host identifier
+	$host_name = $query_string;
+	if( strpos( $host_name, "." ) === false ) {
+		$client_id = $host_name;
+	} else {
+        // trim the domain name
+		$client_id = substr( $host_name, 0, strpos( $host_name, "." ) );
+	}
+      
     $manifest = new Manifest(
-        "ldapserver.example.com",
-        "username",
-        "password",
-        "OU=Munki,DC=example,DC=com"
+        $ldap_server,
+        $ldap_username,
+        $ldap_password,
+        $ldap_basedn
     );
 
     $manifest->addComponents( $manifest_components );
     
-    $manifest->addManualEntry( "catalogs", "prod" );
+    // Always include this catalog
+    $manifest->addManualEntry( "catalogs", "production" );
     
+    // Optionally include any other static inclusions here
+    // Note all included files must include a "/" ie be in a sub-directory
+    // $manifest->addManualEntry( "included_manifests", "included_manifests/standard" );
+
     $manifest->generateLDAPEntries( $client_id );
 
     $manifest_xml = $manifest->GenerateManifest();
